97f169f7005ca62b7a194236b17397e31cc2ee00
munish-garg <munishkr.garg@yahoo.in>
2021-03-05 18:23:39 +0530

    finalCommit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeHeading(){
+    let btn = document.getElementById('button');
+    btn.innerHTML='HOVERED';
+}
+
+function normalHeading(){
+    let btn = document.getElementById('button');
+    btn.innerHTML = 'HOVER ME';
+}
